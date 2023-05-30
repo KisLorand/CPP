da243eb400cda05b2c797ceccab2d44d60b9c55d
KisLorand <kislorand270@gmail.com>
2023-05-30 16:19:40 +0200

    add SimpleSocket object base

---
+++ http_server/Networking/SimpleSocket.hpp
@@ -0,0 +1,29 @@
+
+#ifndef SimpleSocket_hpp
+#define SimpleSocket_hpp
+
+#include <stdio.h>
+
+namespace hde
+{
+    class SimpleSocket
+    {
+    private:
+        /* data */
+    public:
+        SimpleSocket(/* args */);
+        ~SimpleSocket();
+    };
+    
+    SimpleSocket::SimpleSocket(/* args */)
+    {
+    }
+    
+    SimpleSocket::~SimpleSocket()
+    {
+    }
+    
+} // namespace hde
+
+
+#endif /* SimpleSocket_hpp */
