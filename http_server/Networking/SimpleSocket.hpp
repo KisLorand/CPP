93bcb1872ed4503e71f8523b088d2fccbd9ef7bb
KisLorand <kislorand270@gmail.com>
2023-05-31 10:08:16 +0200

    add getter frunctions

--- http_server/Networking/SimpleSocket.hpp
+++ http_server/Networking/SimpleSocket.hpp
@@ -18,8 +18,14 @@ namespace hde
     public:
         SimpleSocket(int domain, int service, int protocol, int port, u_long interface);
         ~SimpleSocket();
+
+        struct sockaddr_in get_address();
+        int get_sock();
+        int get_connection();
+
         virtual int establish_network_connection(int sock, struct sockaddr_in address) = 0;
         void test_connection(int);
+
     };
     
 } // namespace hde
