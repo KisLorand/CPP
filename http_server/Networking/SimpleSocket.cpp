c29a23905f8c41d983aaee05f185235731f4a806
KisLorand <kislorand270@gmail.com>
2023-05-31 09:50:17 +0200

    add socket, change connection

--- http_server/Networking/SimpleSocket.cpp
+++ http_server/Networking/SimpleSocket.cpp
@@ -5,8 +5,11 @@ hde::SimpleSocket::SimpleSocket::SimpleSocket(int domain, int service, int proto
 {
     address.sin_family = domain;
     address.sin_port = htons(port); //int -> bites are in an order for the host system => order for the network : htons function is used for that
-    address.sin_addr.s_addr = interface; //your IP address
-    connection = socket(domain, service, protocol);
+    address.sin_addr.s_addr = htonl(interface); //your IP address, same type of conversion
+
+
+    sock = socket(domain, service, protocol);
+    connection = establish_network_connection(sock, address);
 }
     
 hde::SimpleSocket::~SimpleSocket()
