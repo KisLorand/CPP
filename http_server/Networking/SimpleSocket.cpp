e86078344e30b58a84be9056b0e7ae05a4b370c0
KisLorand <kislorand270@gmail.com>
2023-05-31 09:40:04 +0200

    address values assignment to ctor

--- http_server/Networking/SimpleSocket.cpp
+++ http_server/Networking/SimpleSocket.cpp
@@ -1,8 +1,11 @@
 
 #include "SimpleSocket.hpp"
 
-hde::SimpleSocket::SimpleSocket::SimpleSocket(int domain, int service, int protocol)
+hde::SimpleSocket::SimpleSocket::SimpleSocket(int domain, int service, int protocol, int port, u_long interface)
 {
+    address.sin_family = domain;
+    address.sin_port = htons(port); //int -> bites are in an order for the host system => order for the network : htons function is used for that
+    address.sin_addr.s_addr = interface; //your IP address
     connection = socket(domain, service, protocol);
 }
     
