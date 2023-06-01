307f32a94f653128b239ef439d1e70ff843da0ed
KisLorand <kislorand270@gmail.com>
2023-06-01 14:39:41 +0200

    add base BindingSocket class

---
+++ http_server/Networking/BindingSocket.hpp
@@ -0,0 +1,29 @@
+
+#ifndef BindingSocket_hpp
+#define BindingSocket_hpp
+
+#include <stdio.h>
+
+namespace hde
+{
+    class BindingSocket
+    {
+    private:
+        /* data */
+    public:
+        BindingSocket(/* args */);
+        ~BindingSocket();
+    };
+    
+    BindingSocket::BindingSocket(/* args */)
+    {
+    }
+    
+    BindingSocket::~BindingSocket()
+    {
+    }
+    
+} // namespace hde
+
+
+#endif /* BindingSocket_hpp */
